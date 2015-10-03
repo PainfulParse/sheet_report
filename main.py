0940fcbf913ca83fd6baf91e9481be0b22cd41d2
Brandon <brandonmcghee@gmail.com>
2015-10-02 21:35:23 -0400

    Date Validation and Generate Button disabled

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygal
 from pygal.style import CleanStyle
 import reader
-from flask import Flask, Response , render_template, request, send_file
+from flask import Flask, Response , render_template, request
 from datetime import date, timedelta, datetime
 
 app = Flask(__name__ , static_url_path='')
@@ -9,6 +9,30 @@ app = Flask(__name__ , static_url_path='')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/line/')
+def serve():
+	line_chart = pygal.Line()
+	line_chart.title = 'Browser usage evolution (in %)'
+	line_chart.x_labels = map(str, range(2002, 2013))
+	line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+	line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+	line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+	line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+	return Response(response=line_chart.render(), content_type='image/svg+xml')
+
+@app.route('/line_chart/')
+def display():
+	return """
+	<html>
+    	<body>
+        	<h1>hello pygal</h1>
+        	<figure>
+        		<embed type="image/svg+xml" src="/line/" />
+        	</figure>
+    	</body>
+	</html>'
+	"""
 
 @app.route('/chart/', methods=['POST'])
 def avgChart():
@@ -43,7 +67,6 @@ def avgChart():
 			if key[0:5] in days:
 				if i == 'v1':
 					v1.append(data[key])
-					print(data[key])
 				elif i == 'v2':
 					v2.append(data[key])
 				elif i == 'v3':
@@ -72,10 +95,8 @@ def avgChart():
 	l1   = None
 	p1   = None
 
-	print('Rendering File')
 	chart = line_chart.render(is_unicode=True)
-	line_chart.render_to_file('chart.svg')
-
+	chart_file = line_chart.render_to_file('chart.svg')
 	return chart
 
 if __name__ == '__main__':
